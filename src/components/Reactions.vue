d2b15bbca345ec6409c53e797d4514e72f44ab84
Julianb18 <julian141718@gmail.com>
2021-06-24 18:55:00 +0200

    reactions icons added with styling

--- src/components/Reactions.vue
+++ src/components/Reactions.vue
@@ -1,17 +1,44 @@
 <template>
-  <div class="border flex justify-center">
+  <div class="border flex justify-center ">
     <div
       class="flex flex-wrap justify-center my-10 border-2 border-red-500 max-w-lg"
     >
-      Icons here
+      <div v-for="reaction in reactionList" :key="reaction.id">
+        <div class="my-4 px-5">
+          <div
+            @click="$store.commit('increaseLikes')"
+            class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+          >
+            <img :src="reaction.icon" :alt="reaction.reaction" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Reactions',
-}
-</script>
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-<style></style>
+export default defineComponent({
+  name: 'Reactions',
+  data() {
+    return {
+      reactionList: [
+        { id: 1, reaction: 'thumbsUp', icon: require('../assets/like.png') },
+        {
+          id: 2,
+          reaction: 'thumbsDown',
+          icon: require('../assets/dislike.png'),
+        },
+        { id: 3, reaction: 'heart', icon: require('../assets/heart.png') },
+        { id: 4, reaction: 'clap', icon: require('../assets/clapping.png') },
+        { id: 5, reaction: 'happy', icon: require('../assets/happy.png') },
+        { id: 6, reaction: 'excited', icon: require('../assets/excited.png') },
+        { id: 7, reaction: 'awesome', icon: require('../assets/rocket.png') },
+        { id: 8, reaction: 'sad', icon: require('../assets/sad.png') },
+      ],
+    }
+  },
+})
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1b7fbb',
+      },
+      boxShadow: {
+        primaryIcon: '8px 8px 16px #d9d9d9,-8px -8px 16px #ffffff;',
+        pressedIcon: 'inset 8px 8px 16px #d9d9d9,inset -8px -8px 16px #ffffff',
+      },
+    },
   },
   variants: {
     extend: {},

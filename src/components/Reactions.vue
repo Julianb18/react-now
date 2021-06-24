61924b66f2b4e35038667178d52891f06b49552d
Julianb18 <julian141718@gmail.com>
2021-06-24 19:32:36 +0200

    reaction icon functionality, implemented vuex store and localstorage

--- src/components/Reactions.vue
+++ src/components/Reactions.vue
@@ -3,16 +3,133 @@
     <div
       class="flex flex-wrap justify-center my-10 border-2 border-red-500 max-w-lg"
     >
-      <div v-for="reaction in reactionList" :key="reaction.id">
+      <!-- <div v-for="reaction in $store.state.reactionList" :key="reaction.id">
+      this will wrap one ov the divs to dynamically load them
         <div class="my-4 px-5">
-          <div
-            @click="$store.commit('increaseLikes')"
-            class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
-          >
-            <img :src="reaction.icon" :alt="reaction.reaction" />
-          </div>
+        <div
+          @click="$store.commit('increaseLikes')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img :src="reaction.icon" alt="" />
         </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.likes }}
+        </span>
       </div>
+      </div> -->
+
+      <!-- TODO: make this code more dynamic like above  -->
+      <!-- like -->
+      <div class="my-4 px-5">
+        <div
+          @click="$store.commit('increaseLikes')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img src="../assets/like.png" alt="" />
+        </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.likes }}
+        </span>
+      </div>
+      <!-- end of like -->
+
+      <!-- dislike -->
+      <div class="my-4 px-5">
+        <div
+          @click="$store.commit('increaseDislikes')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img src="../assets/dislike.png" alt="" />
+        </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.dislikes }}
+        </span>
+      </div>
+      <!-- end of dislike -->
+
+      <!-- heart -->
+      <div class="my-4 px-5">
+        <div
+          @click="$store.commit('increaseHearts')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img src="../assets/heart.png" alt="" />
+        </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.hearts }}
+        </span>
+      </div>
+      <!-- end of heart -->
+
+      <!-- clap -->
+      <div class="my-4 px-5">
+        <div
+          @click="$store.commit('increaseClaps')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img src="../assets/clapping.png" alt="" />
+        </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.claps }}
+        </span>
+      </div>
+      <!-- end of clap -->
+
+      <!-- happy -->
+      <div class="my-4 px-5">
+        <div
+          @click="$store.commit('increaseSmiles')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img src="../assets/happy.png" alt="" />
+        </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.smiles }}
+        </span>
+      </div>
+      <!-- end of happy -->
+
+      <!-- excited -->
+      <div class="my-4 px-5">
+        <div
+          @click="$store.commit('increaseGrins')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img src="../assets/excited.png" alt="" />
+        </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.grins }}
+        </span>
+      </div>
+      <!-- end of excited -->
+
+      <!-- awesome -->
+      <div class="my-4 px-5">
+        <div
+          @click="$store.commit('increaseRockets')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img src="../assets/rocket.png" alt="" />
+        </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.rockets }}
+        </span>
+      </div>
+      <!-- end of awesome -->
+
+      <!-- sad -->
+      <div class="my-4 px-5">
+        <div
+          @click="$store.commit('increaseSaddies')"
+          class="bg-white rounded-md p-6 my-3 shadow-primaryBtn hover:shadow-pressedBtn cursor-pointer"
+        >
+          <img src="../assets/sad.png" alt="" />
+        </div>
+        <span class="bg-white p-1 block rounded-md font-bold text-primary">
+          {{ $store.state.counters.saddies }}
+        </span>
+      </div>
+      <!-- end of sad -->
     </div>
   </div>
 </template>
@@ -22,23 +139,5 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'Reactions',
-  data() {
-    return {
-      reactionList: [
-        { id: 1, reaction: 'thumbsUp', icon: require('../assets/like.png') },
-        {
-          id: 2,
-          reaction: 'thumbsDown',
-          icon: require('../assets/dislike.png'),
-        },
-        { id: 3, reaction: 'heart', icon: require('../assets/heart.png') },
-        { id: 4, reaction: 'clap', icon: require('../assets/clapping.png') },
-        { id: 5, reaction: 'happy', icon: require('../assets/happy.png') },
-        { id: 6, reaction: 'excited', icon: require('../assets/excited.png') },
-        { id: 7, reaction: 'awesome', icon: require('../assets/rocket.png') },
-        { id: 8, reaction: 'sad', icon: require('../assets/sad.png') },
-      ],
-    }
-  },
 })
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,123 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    // store the data of our app
+    counters: {
+      likes: localStorage.getItem('likes')
+        ? Number(localStorage.getItem('likes'))
+        : 0,
+      dislikes: localStorage.getItem('dislikes')
+        ? Number(localStorage.getItem('dislikes'))
+        : 0,
+      hearts: localStorage.getItem('hearts')
+        ? Number(localStorage.getItem('hearts'))
+        : 0,
+      claps: localStorage.getItem('claps')
+        ? Number(localStorage.getItem('claps'))
+        : 0,
+      smiles: localStorage.getItem('smiles')
+        ? Number(localStorage.getItem('smiles'))
+        : 0,
+      grins: localStorage.getItem('grins')
+        ? Number(localStorage.getItem('grins'))
+        : 0,
+      rockets: localStorage.getItem('rockets')
+        ? Number(localStorage.getItem('rockets'))
+        : 0,
+      saddies: localStorage.getItem('saddies')
+        ? Number(localStorage.getItem('saddies'))
+        : 0,
+    },
+
+    // TODO: find a way to get the icon data from store
+    // reactionList: [
+    //   {
+    //     id: 1,
+    //     reaction: 'thumbsUp',
+    //     icon: require('../assets/like.png'),
+    //   },
+    //   {
+    //     id: 2,
+    //     reaction: 'thumbsDown',
+    //     icon: require('../assets/dislike.png'),
+    //   },
+    //   {
+    //     id: 3,
+    //     reaction: 'heart',
+    //     icon: require('../assets/heart.png'),
+    //   },
+    //   {
+    //     id: 4,
+    //     reaction: 'clap',
+    //     icon: require('../assets/clapping.png'),
+    //   },
+    //   { id: 5, reaction: 'happy', icon: require('../assets/happy.png') },
+    //   {
+    //     id: 6,
+    //     reaction: 'excited',
+    //     icon: require('../assets/excited.png'),
+    //   },
+    //   {
+    //     id: 7,
+    //     reaction: 'awesome',
+    //     icon: require('../assets/rocket.png'),
+    //   },
+    //   {
+    //     id: 8,
+    //     reaction: 'sad',
+    //     icon: require('../assets/sad.png'),
+    //   },
+    // ],
   },
   mutations: {
+    // TODO: Find away to make this and state above more dynamic
+    // methods which change data in the state
+    increaseLikes(state) {
+      state.counters.likes++
+      window.localStorage.setItem('likes', JSON.stringify(state.counters.likes))
+    },
+    increaseDislikes(state) {
+      state.counters.dislikes++
+      window.localStorage.setItem(
+        'dislikes',
+        JSON.stringify(state.counters.dislikes)
+      )
+    },
+    increaseHearts(state) {
+      state.counters.hearts++
+      window.localStorage.setItem(
+        'hearts',
+        JSON.stringify(state.counters.hearts)
+      )
+    },
+    increaseClaps(state) {
+      state.counters.claps++
+      window.localStorage.setItem('claps', JSON.stringify(state.counters.claps))
+    },
+    increaseSmiles(state) {
+      state.counters.smiles++
+      window.localStorage.setItem(
+        'smiles',
+        JSON.stringify(state.counters.smiles)
+      )
+    },
+    increaseGrins(state) {
+      state.counters.grins++
+      window.localStorage.setItem('grins', JSON.stringify(state.counters.grins))
+    },
+    increaseRockets(state) {
+      state.counters.rockets++
+      window.localStorage.setItem(
+        'rockets',
+        JSON.stringify(state.counters.rockets)
+      )
+    },
+    increaseSaddies(state) {
+      state.counters.saddies++
+      window.localStorage.setItem(
+        'saddies',
+        JSON.stringify(state.counters.saddies)
+      )
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
 })

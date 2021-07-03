b8a3e24bde18669d76f4da4d1bbee71e9d1f08ae
Julianb18 <julian141718@gmail.com>
2021-07-03 09:21:47 +0200

    removed pluggin for screen width

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { VueWindowSizePlugin } from 'vue-window-size/option-api'
+
 import './assets/tailwind.css'
 
 createApp(App)
   .use(store)
   .use(router)
-  .use(VueWindowSizePlugin)
   .mount('#app')
